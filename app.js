7e86a205748dc9cecfebae6742733942cde37d24
Wafa Mohammed <wafamd00@gmail.com>
2026-07-23 20:56:24 +0530

    Add Express app, test, and Dockerfile

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Hello from my CI/CD pipeline!'));
+app.get('/health', (req, res) => res.status(200).send('OK'));
+app.listen(3000, () => console.log('Running on port 3000'));
